1fbb61020cb60b3a4fbe01d531af4d02e790f771
Michael Shiver <michael.shiver@ascte.org>
2026-09-22 10:14:30 -0500

    Added new song

--- playlist.py
+++ playlist.py
@@ -4,6 +4,7 @@ SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
     ("Viva La Vida", "Coldplay"),
+    ("Still Alive", "Jonothan Colton"),
 ]
 
 def print_playlist(songs):
